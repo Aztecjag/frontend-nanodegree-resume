8b7fbe00542e56c4b5c14e9d4ac6d65b75d29eb4
Aztecjag <Aztecjag@users.noreply.github.com>
2015-01-10 14:42:42 -0500

    Update resumeBuilder.js

---
+++ js/resumeBuilder.js
@@ -0,0 +1,4 @@
+var formattedName = HTMLheaderName.replace("%data%", "Efren Espinosa"); 
+$("#header").prepend(formattedName);
+var formattedRole = HTMLheaderRole.replace("%data%", "Front-end Web Developer");
+$("#header").append(formattedRole);
